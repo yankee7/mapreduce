7cbc703775970fcfc347149bcec02fe86fc51d7c
Andrew Yanovych <yankee7@quintagroup.com>
2015-08-25 12:07:20 +0300

    no function

--- mapper.py
+++ mapper.py
@@ -4,27 +4,29 @@
 import sys
 
 
-def mapper():
-    """ this method checks if there is buildings higher then 10m or 3 stores """
-    building = []  # initalize the building variable
-    for line in sys.stdin:
+#def mapper():
+#    """ this method checks if there is buildings higher then 10m or 3 stores """
+#according to few articles there' s no need to make a function, anyway I can do so
 
-        # do only for  <way /> tag, it often starts building attributes
-        if "<way" in line:
-            # gets the  id of <way /> tag
-            building = [strng[4:-1] for strng in line.split(' ') if strng.startswith('id')]
-        elif "<tag" in line:
-            # removes parts of tag leaves only attributes
-            line = line.replace("<tag", "").replace("/>", "").replace('\n', '')
-            # removes attributes keys of tag
-            line = line.replace('k=', '').replace('v=', '')
-            # converting to dict from tag attributes
-            tag = dict([' '.join(line.split()).replace('"', '').split(" ", 1)])
-            # searches if there is needed attribute in tag dict and checks its value
-        if 'building:levels' in tag:
-            if tag['building:levels'] >= 3:
-                print building
+building = []  # initalize the building variable
+for line in sys.stdin:
+
+    # do only for  <way /> tag, it often starts building attributes
+    if "<way" in line:
+        # gets the  id of <way /> tag
+        building = [strng[4:-1] for strng in line.split(' ') if strng.startswith('id')]
+    elif "<tag" in line:
+        # removes parts of tag leaves only attributes
+        line = line.replace("<tag", "").replace("/>", "").replace('\n', '')
+        # removes attributes keys of tag
+        line = line.replace('k=', '').replace('v=', '')
+        # converting to dict from tag attributes
+        tag = dict([' '.join(line.split()).replace('"', '').split(" ", 1)])
         # searches if there is needed attribute in tag dict and checks its value
-        elif 'height' in tag:
-            if tag['height'] > 10:
-                print building
+    if 'building:levels' in tag:
+        if tag['building:levels'] >= 3:
+            print building
+    # searches if there is needed attribute in tag dict and checks its value
+    elif 'height' in tag:
+        if tag['height'] > 10:
+            print building
